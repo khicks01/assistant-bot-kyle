860d346032953c08a76fdb2f0cb00b1140d1e55a
Kyle Hicks <kylekhicks@gmail.com>
2020-09-07 13:51:23 -0500

    added database logging

--- actions/views.py
+++ actions/views.py
@@ -1,9 +1,14 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 import json
-from django.http import HttpResponse, JsonResponse
 import slack
+
+from .models import SlackPost
+
+
+
 @csrf_exempt
 def event_hook(request):
     client = slack.WebClient(token=settings.BOT_USER_ACCESS_TOKEN)
@@ -20,8 +25,12 @@ def event_hook(request):
             user = event_msg['user']
             message_timestamp = event_msg['ts']
             if(user != 'U01ACS227RS'):
+                new_db_entry = SlackPost()
                 channel = event_msg['channel']
-                response_msg = "I hear you"
+                new_db_entry.time_stamp = message_timestamp
+                new_db_entry.user = user
+                new_db_entry.user_request = event_msg['text']
+                response_msg = "I added this request to the database"
                 client.chat_postMessage(channel=channel, thread_ts= message_timestamp, text=response_msg)
                 return HttpResponse(status=200)
     return HttpResponse(status=200)
