3ccca6385487769c1c072e23405a15aa5c45aaff
Kyle Hicks <kylekhicks@gmail.com>
2020-09-07 14:43:29 -0500

    revert changes

--- actions/views.py
+++ actions/views.py
@@ -13,28 +13,28 @@ from .models import SlackPost
 def event_hook(request):
     client = slack.WebClient(token=settings.BOT_USER_ACCESS_TOKEN)
     json_dict = json.loads(request.body.decode('utf-8'))
+    #check if this is a verification challenge
     if json_dict['token'] != settings.VERIFICATION_TOKEN:
         return HttpResponse(status=403)
     if 'type' in json_dict:
         if json_dict['type'] == 'url_verification':
             response_dict = {"challenge": json_dict['challenge']}
             return JsonResponse(response_dict, safe=False)
+    #if this is a normal event which the bot is subscribed to
     if 'event' in json_dict:
         event_msg = json_dict['event']
         if event_msg['type'] == 'message':
             user = event_msg['user']
             message_timestamp = event_msg['ts']
+            #Make sure the post is not coming from the bot itself
             if(user != 'U01ACS227RS'):
-                new_db_entry = SlackPost()
                 channel = event_msg['channel']
+                new_db_entry = SlackPost()
                 new_db_entry.time_stamp = message_timestamp
                 new_db_entry.user = user
-                if(new_db_entry.objects.filter(user_requests = event_msg['text'])).exists():
-                    response_msg = "Someone has already asked this question"
-                else:    
-                    new_db_entry.user_request = event_msg['text']
-                    new_db_entry.save()
-                    response_msg = "I added this request to the database"
+                new_db_entry.user_request = event_msg['text']
+                new_db_entry.save()
+                response_msg = "I added this request to the database"
                 client.chat_postMessage(channel=channel, thread_ts= message_timestamp, text=response_msg)
                 return HttpResponse(status=200)
     return HttpResponse(status=200)
