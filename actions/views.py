cb1e274c957a07aef824255fde9e4840f07a1009
Kyle Hicks <kylekhicks@gmail.com>
2020-09-07 12:41:19 -0500

    remove

--- actions/views.py
+++ actions/views.py
@@ -16,16 +16,13 @@ def event_hook(request):
             response_dict = {"challenge": json_dict['challenge']}
             return JsonResponse(response_dict, safe=False)
     
-    if 'bot_id' in json_dict:
-        return HttpResponse(status=200)
-
     if 'event' in json_dict:
         event_msg = json_dict['event']
         print(event_msg)
 
         if event_msg['type'] == 'message':
             user = event_msg['user']
-            if(user != U01ACS227RS):
+            if(user != 'U01ACS227RS'):
                 channel = event_msg['channel']
                 response_msg = "payload: <@%s>" % event_msg
                 client.chat_postMessage(channel=channel, text=response_msg)
