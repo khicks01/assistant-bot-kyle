b3dacc0d5623cd277b49d7ee374cff6d86adaf3f
Kyle Hicks <kylekhicks@gmail.com>
2020-09-07 13:55:04 -0500

    viewing the db on the backend

--- actions/admin.py
+++ actions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import SlackPost
 # Register your models here.
+
+class SlackPostAdmin(admin.ModelAdmin):
+    list_display = ('time_stamp' , 'user_request' , 'user')
+
+admin.site.register(SlackPost, SlackPostAdmin)
