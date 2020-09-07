97f02229c32eb14ae8aee22f3994928dc3dd40a7
Kyle Hicks <kylekhicks@gmail.com>
2020-09-07 13:02:24 -0500

    try to get a reply

--- actions/views.py
+++ actions/views.py
@@ -18,10 +18,11 @@ def event_hook(request):
         event_msg = json_dict['event']
         if event_msg['type'] == 'message':
             user = event_msg['user']
-            thread_value = event_msg['thread_ts']
+            message_timestamp = event_msg['ts']
             if(user != 'U01ACS227RS'):
                 channel = event_msg['channel']
                 response_msg = "I hear you"
-                client.chat_postMessage(channel=channel, thread_ts= thread_value, text=response_msg)
+                client.conversations_replies(channel= channel, ts= message_timestamp,text=response_msg )
+                #client.chat_postMessage(channel=channel, thread_ts= thread_value, text=response_msg)
                 return HttpResponse(status=200)
     return HttpResponse(status=200)
