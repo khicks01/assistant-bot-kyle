1d4a4f6ddc0bab139ebe8b3b100a9e601c5c0e2b
Kyle Hicks <kylekhicks@gmail.com>
2020-09-08 22:46:07 -0500

    refactor to make it compact / modular

--- actions/views.py
+++ actions/views.py
@@ -15,7 +15,7 @@ def event_hook(request):
     client = slack.WebClient(token=settings.BOT_USER_ACCESS_TOKEN)
     json_dict = json.loads(request.body.decode('utf-8'))
     #check if this is a verification challenge
-    event_sub_challenge_check(json_dict, request)
+    respond_to_subscription_challenge(json_dict, request)
     #if this is a normal event which the bot is subscribed to
     if 'event' in json_dict:
         event_msg = json_dict['event']
@@ -23,39 +23,17 @@ def event_hook(request):
             user = event_msg['user']
             #Make sure the post is not coming from the bot itself
             if(user != 'U01ACS227RS'):
-                message_timestamp = event_msg['ts']
-                channel = event_msg['channel']
-                text = event_msg['text'].lower().strip()
+                message_timestamp, channel, text = gather_message_data(event_msg)
                 words = text.split(" ")
                 answer_msg = []
-                for each_word in words:
-                    try:
-                        print(each_word)
-                        helpful_links = AnswersDatabase.objects.get(keywords__icontains=each_word)
-                        print(helpful_links)
-                        if helpful_links.resource not in answer_msg:
-                            answer_msg.append(helpful_links.resource)
-                    except AnswersDatabase.DoesNotExist:
-                        print("no value found")
-                
-                #message receipt and logging
-                obj, created = SlackPost.objects.get_or_create(user_request= text)
-                if(created):
-                    response_msg = "I added this request to the request database"
-                else:
-                    response_msg = "Already part of the database"
-                client.chat_postMessage(channel=channel, thread_ts= message_timestamp, text=response_msg)
-
+                #build array of helpful links based on the key word
+                answer_msg = find_helpful_links(words, answer_msg)
+                #log the user question
+                SlackPost.objects.get_or_create(user_request= text)
                 # answer given by bot
-                if len(answer_msg) != 0:
-                    client.chat_postMessage(channel=channel, thread_ts= message_timestamp, text=answer_msg)
-                else:
-                    answer_msg = "We didn't find a helpful link for your query, sorry"
-                    client.chat_postMessage(channel=channel, thread_ts= message_timestamp, text=answer_msg)
-                return HttpResponse(status=200)
+                respond_from_bot(answer_msg, client, channel, message_timestamp)
     return HttpResponse(status=200)
-
-def event_sub_challenge_check(json_dict, request):
+def respond_to_subscription_challenge(json_dict, request):
     json_dict = json.loads(request.body.decode('utf-8'))
     if json_dict['token'] != settings.VERIFICATION_TOKEN:
         return HttpResponse(status=403)
@@ -63,3 +41,29 @@ def event_sub_challenge_check(json_dict, request):
         if json_dict['type'] == 'url_verification':
             response_dict = {"challenge": json_dict['challenge']}
             return JsonResponse(response_dict, safe=False)
+def find_helpful_links(user_request_keyword_array, answer_list):
+    for each_word in user_request_keyword_array:
+        try:
+            print(each_word)
+            helpful_links = AnswersDatabase.objects.get(keywords__icontains=each_word)
+            print(helpful_links)
+            if helpful_links.resource not in answer_list:
+                answer_list.append(helpful_links.resource)
+        except AnswersDatabase.DoesNotExist:
+            print("no value found")
+    return answer_list
+def respond_from_bot(bot_answer, slack_client, slack_channel, time_stamp):
+    if len(bot_answer) != 0:
+        slack_client.chat_postMessage(channel=slack_channel, thread_ts= time_stamp, text=bot_answer)
+    else:
+        answer_msg = "We didn't find a helpful link for your query, sorry"
+        slack_client.chat_postMessage(channel=slack_channel, thread_ts= time_stamp, text=answer_msg)
+    return HttpResponse(status=200)
+def gather_message_data(message_json):
+    message_timestamp = message_json['ts']
+    channel = message_json['channel']
+    text = message_json['text'].lower().strip()
+    return message_timestamp, channel, text
+
+
+    
